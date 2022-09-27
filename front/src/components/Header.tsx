23342acd9ce0389d60e490bee51202177e0f42b6
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-09-27 09:03:33 +0900

    add header , footer is making

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function Header() {
+import { ClassAttributes, FC, LiHTMLAttributes } from 'react';
+import style from '../styles/header.module.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+
+const Header: FC = (props) => {
+
   return (
-    <div>
-        
-    </div>
-  );
-}
+    <header>
+      <div className={style.header}>
+        <ul>         
+            <li  className={"/" ? style.current : undefined}>
+              메인 페이지
+            </li>
+            <li className={"/MyPage" ? style.current : undefined}>
+              내 페이지
+            </li>
+            <li className={ "/TestView" ? style.current : undefined}>
+              테스트 뷰
+            </li>
+            <li className={ "/Creator" ? style.current : undefined}>
+              만들기 +
+            </li>
+        </ul>
+      </div>
+    </header>
+  )
+};
+
 
 export default Header;
